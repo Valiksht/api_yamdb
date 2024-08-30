9ac4773fae6fd0e781d3dd9dbea5bec833f789ae
Alina Gorbova <wowanikol1@gmail.com>
2024-08-30 23:11:20 +0300

    Добавлены докстринги поправлены сериализатор тайтлов Поправлены вью тайтлов и жаноров поправлены пермишены

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,45 +4,59 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
 class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор для модели категории (Category)."""
+
     class Meta:
-        fields = '__all__'
         model = Category
-
-
-class GenreSerializer (serializers.ModelSerializer):
-    class Meta:
         fields = '__all__'
-        model = Genre
 
+class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели жанра (Genre)."""
+
+    class Meta:
+        model = Genre
+        fields = '__all__'
 
 class TitleSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = '__all__'
-        model = Title
+    """Сериализатор для модели произведения (Title)."""
 
+    genre = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Genre.objects.all()
+    )
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+    def validate(self, data):
+        return data
 
 class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели отзыва (Review)."""
+
     class Meta:
         model = Review
         fields = ['id', 'title', 'text', 'author', 'score', 'pub_date']
         read_only_fields = ['pub_date', 'author']
 
-
 class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели комментария (Comment)."""
+
     class Meta:
         model = Comment
         fields = ['id', 'review', 'text', 'author', 'pub_date']
         read_only_fields = ['pub_date', 'author']
 
-
-class UserSerializer (serializers.ModelSerializer):
-    """Сериализатор модели пользователя."""
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели пользователя (User)."""
 
     class Meta:
         model = User
-        # fields = '__all__'
         fields = (
             'username', 'email', 'first_name', 'last_name', 'bio', 'role'
         )
