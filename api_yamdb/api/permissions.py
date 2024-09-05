4f53cabb0d359a88ff74e956fd28d828285e27e5
Valentin Rubanov <rubanov.valentin@ya.ru>
2024-09-06 01:01:17 +0300

    reviev 1.1

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -31,11 +31,11 @@ class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if request.user.is_authenticated:
-            return request.user.is_admin or request.user.is_superuser
+            return request.user.is_admin
 
     def has_object_permission(self, request, view, obj):
         if request.user.is_authenticated:
-            return request.user.is_admin or request.user.is_superuser
+            return request.user.is_admin
         return False
 
 
---
+++ api_yamdb/reviews/validators.py
@@ -0,0 +1,9 @@
+from django.core.validators import RegexValidator
+
+simbol_validate = RegexValidator(
+        regex=r'^[\w.@+-]+$',
+        message=('Введите допустимое имя пользователя.'
+                 'Допустимые символы только буквы цифры'
+                 'и символы "@", ".", "+", "-", "_".'),
+        code='invalid_username'
+    )
